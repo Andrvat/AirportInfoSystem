bba3dd65f4e8c21e4ea399e313057a9d3b65b065
andrvat <andreivalitov2001@gmail.com>
2022-03-19 20:41:21 +0700

    Delete unused object in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            new Ticket();
             OracleDbProvider provider = new OracleDbProvider();
             DbModel model = new DbModel();
             ControllerManager controllerManager = ControllerManager.builder()
