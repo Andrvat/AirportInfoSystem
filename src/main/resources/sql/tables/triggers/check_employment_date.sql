60e17c5289acc1f450d8cd302607dd3cade340df
andrvat <andreivalitov2001@gmail.com>
2022-05-17 15:18:11 +0700

    Add new check triggers for correct dates

---
+++ src/main/resources/sql/tables/triggers/check_employment_date.sql
@@ -0,0 +1,9 @@
+create or replace TRIGGER CHECK_EMPLOYMENT_DATE
+    BEFORE INSERT OR UPDATE OF EMPLOYMENT_DATE
+    ON EMPLOYEE
+    FOR EACH ROW
+    WHEN (NEW.BIRTH_DATE >= CURRENT_DATE OR NEW.BIRTH_DATE < TO_DATE('1970/01/01', 'yyyy/mm/dd'))
+BEGIN
+    raise_application_error(-20000,
+                            'Employment date cannot be greater than current date and less than of airport date founding');
+END;
